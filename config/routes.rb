e325334ae9c2b39089f7ea25c8b1b9edeadc6162
Michael <mpkelley08@gmail.com>
2018-01-14 11:02:52 -0500

    chkpnt28 comments: completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,15 @@
 Rails.application.routes.draw do
 
-  # get 'sponsored_posts/show'
-  #
-  # get 'sponsored_posts/new'
-  #
-  # get 'sponsored_posts/edit'
-
-  # get 'questions/index'
-  #
-  # get 'questions/show'
-  #
-  # get 'questions/new'
-  #
-  # get 'questions/edit'
-
   resources :questions
 
   resources :topics do
     resources :posts, except: [:index]
-    resources :sponsored_posts, except: [:index]
+      resources :sponsored_posts, except: [:index]
   end
 
+  resources :posts, only: [] do
+   resources :comments, only: [:create, :destroy]
+  end
   resources :users, only: [:new, :create]
 
   resources :sessions, only: [:new, :create, :destroy]
